4f8e426909cf9a6dbfe7e0f6ce0f3334ce758870
Lunad129 <ict21014@std.uwu.ac.lk>
2026-07-02 21:21:55 +0530

    feat: enhance Tasks page with improved styling, task management features, and icon integration

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import "./Tasks.css";
+import { FiPlus } from "react-icons/fi";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 function Tasks() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState("");
+
+  //edit function
+  const startEdit = (index) => {
+  setEditIndex(index);
+  setEditText(tasks[index]);
+  };
 
   //addTask function
   const addTask = () => {
@@ -13,20 +24,44 @@ function Tasks() {
   setTask("");
   };
 
-
+  //deleteTask function
+  const deleteTask = (index) => {
+  const confirmDelete = window.confirm("Are you sure you want to delete this task?");
+  if (!confirmDelete) return;
+  const newTasks = tasks.filter((_, i) => i !== index);
+  setTasks(newTasks);
+};
+  
+  
   return (
   <>
   <div className="task-app">
-    <h1>Tasks Page</h1>
+    <h1 className="head">Tasks</h1>
 
     <div className="input-box">
       <input 
         type="text" 
         placeholder="Enter a task..."
         value={task}
-        onChange={(e) => setTask(e.target.value)}/>
+        onChange={(e) => setTask(e.target.value)}
+        onKeyDown={(e) => {if (e.key === "Enter") {addTask();}
+      }}/>
 
-      <button onClick={addTask} disabled={task === ""}>Add</button>
+      <button className="add-button" onClick={addTask} disabled={task === ""}><FiPlus /></button>
+      
+
+    </div>
+    
+    <div className="task-container">
+        {tasks.map((t, index) => (
+          <div key={index} className="task-card">
+              {t}
+              <div className="task-actions">
+                <FaEdit className="icon edit-icon" onClick={() => startEdit(index)}/>
+                <FaTrash className="icon delete-icon" onClick={() => deleteTask(index)}/>
+              </div>
+          </div>
+      ))}
     </div>
   </div>
   </>  
